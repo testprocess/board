5b9c4640353fc7e2fe5a8767595e0278ae95c429
Dipokal HHJ <hhj@devent.kr>
2023-07-22 15:31:28 +0900

    feat: delete feed

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import dds from 'deventds/dist/handle'
 import Cookies from 'js-cookie'
-import { TextField, Button, Stack, Grid, Card, CardContent, Typography, Box, Skeleton, IconButton, Avatar } from '@mui/material';
+import { TextField, Button, Stack, Grid, Card, CardContent, Typography, Box, Skeleton, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Popup } from './Alert'
@@ -42,18 +42,20 @@ async function insertFeed(content) {
 }
 
 
-// async  delete(idx) {
-//     let response = await fetch("/api/feeds/"+idx, {
-//         method: "DELETE",
-//         headers: {
-//             "Content-Type": "application/x-www-form-urlencoded",
-//             "x-access-token": this.token
-//         }
-//     });
+async function deleteFeed(idx) {
+    let token = Cookies.get("user")
 
-//     let data = response.json();
-//     return data;
-// }
+    let response = await fetch("/api/feeds/"+idx, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-access-token": token
+        }
+    });
+
+    let data = response.json();
+    return data;
+}
 
 // async update(idx, content) {
 //     let response = await fetch("/api/feeds/"+idx, {
@@ -75,8 +77,6 @@ function Feed() {
     const [fetching, setFetching] = useState(0);
     const [fetchingLock, setFetchingLock] = useState(false);
     const [fetchingStop, setFetchingStop] = useState(false);
-
-
 
     const checkLogin = () => {
         let token = Cookies.get("user")
@@ -109,7 +109,6 @@ function Feed() {
         }
     }
     
-
     useEffect(() => {
         let loginStatus = checkLogin()
         setLoginStatus(loginStatus.isVaild)
@@ -134,9 +133,7 @@ function Feed() {
         }
     }, [fetching])
 
-
     document.addEventListener('scroll', handleScroll)
-
 
     if (isLogin) {
         return (
@@ -161,22 +158,22 @@ function Feed() {
 
     return (
         <Grid container sx={{ marginTop: "1rem" }} spacing={3}>
-        <Grid item xs md>
-        </Grid>
-        <Grid item xs={10} md={6}>
-            {feeds.map(feed => (
-                <FeedBody feed={feed}></FeedBody>
+            <Grid item xs md>
+            </Grid>
+            <Grid item xs={10} md={6}>
+                {feeds.map(feed => (
+                    <FeedBody feed={feed}></FeedBody>
 
-            ))}
-            <FeedSkeleton></FeedSkeleton>
-
-        </Grid>
-        <Grid item xs md>
-        </Grid>
+                ))}
+                <FeedSkeleton></FeedSkeleton>
+            </Grid>
+            <Grid item xs md>
+            </Grid>
         </Grid>
     );
 
 }
+
 
 function FeedInput(props) {
     const [input, setInput] = useState('')
@@ -194,14 +191,11 @@ function FeedInput(props) {
 
         if (input.length == 0) {
             return 0
-
         }
     
         props.feed.setFeeds([{idx: props.feed.feeds[0].idx + 1, content: input, owner: '', date: new Date()}, ...props.feed.feeds])
-
         insertFeed(input)
         setInput('')
-
     }
 
     return (
@@ -217,10 +211,10 @@ function FeedInput(props) {
             <Typography sx={{ fontSize: "0.8rem", textAlign: 'right', color: input.length < 990 ? "#000000" : "#fc4242"  }}>{input.length}/1000</Typography>
             <Button variant="contained" onClick={handleClick}><SendIcon /> </Button>
             <Popup trigger={alertTrigger} message="길이가 너무 길어요" severity="info"></Popup>
-
         </Stack>
     );
 }
+
 
 function FeedBody({ feed }) {
     if (feed.type == 0) {
@@ -235,34 +229,93 @@ function FeedBody({ feed }) {
 
                 <Box sx={{ fontSize: 14, whiteSpace: 'pre-line', wordWrap: 'break-word' }} color="text.secondary">
                     {feed.content}
-
                 </Box>
             </CardContent>
         </Card>
     )
 }
 
+
 function FeedProfile({ feed }) {
     return (
-        <Box sx={{ flexGrow: 1, overflow: 'hidden', marginBottom: "1rem" }}>
-          <Grid container wrap="nowrap" spacing={2}>
-            <Grid item>
-              <Avatar sx={{ width: '2rem', height: '2rem', fontSize: '1rem' }}>{feed.owner.slice(0, 1)}</Avatar>
-            </Grid>
-            <Grid item xs zeroMinWidth sx={{ alignContent: 'center'}}>
-              <Typography sx={{ fontSize: '1rem' }} noWrap>{feed.owner}</Typography>
-            </Grid>
-            <Grid item xs zeroMinWidth sx={{ justifyContent: 'flex-end',  }}>
-              <Typography sx={{ textAlign: 'right'}} noWrap>
-              <IconButton aria-label="delete" size="small">
-              <MoreVertIcon sx={{ fontSize: '1.2rem' }}></MoreVertIcon>
-                </IconButton>
-</Typography>
-            </Grid>
-          </Grid>   
+        <Box sx={{ flexGrow: 1, overflow: 'hidden', marginBottom: "1rem", alignContent: 'center' }}>
+            <Grid container wrap="nowrap" spacing={2} sx={{ alignContent: 'center', alignItems: 'center' }}>
+                <Grid item>
+                    <Avatar sx={{ width: '2rem', height: '2rem', fontSize: '1rem' }}>{feed.owner.slice(0, 1)}</Avatar>
+                </Grid>
+                <Grid item xs zeroMinWidth sx={{ alignContent: 'center'}}>
+                    <Typography sx={{ fontSize: '1rem' }} noWrap>{feed.owner}</Typography>
+                    <Typography sx={{ fontSize: '0.7rem' }} color="text.secondary" noWrap>{feed.date}</Typography>
+
+                </Grid>
+                <Grid item xs zeroMinWidth sx={{ justifyContent: 'flex-end',  }}>
+                    <Typography sx={{ textAlign: 'right'}} noWrap>
+                        <FeedMenu feed={feed}></FeedMenu>
+                    </Typography>
+                </Grid>
+            </Grid>   
         </Box>
     )
 }
+
+
+function FeedMenu({ feed }) {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [alertTrigger, setAlertTrigger] = useState(0)
+
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const handleDelete = async () => {
+        const deleteThisFeed = await deleteFeed(feed.idx)
+
+        console.log(deleteThisFeed)
+
+        if (deleteThisFeed.status == 1) {
+            setAlertTrigger(alertTrigger + 1)
+        }
+
+        handleClose()
+    }
+
+    return (
+        <>
+        <IconButton
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick} 
+        aria-label="delete" 
+        size="small">
+            <MoreVertIcon sx={{ fontSize: '1.2rem' }}></MoreVertIcon>
+        </IconButton>
+
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            }}>
+
+            <MenuItem sx={{ color: "#e64840" }} onClick={handleDelete}>delete</MenuItem>
+        </Menu>
+
+        <Popup trigger={alertTrigger} message="삭제 완료" severity="success"></Popup>
+
+        </>
+
+    )
+}
+
 
 function FeedSkeleton() {
     return (
@@ -270,11 +323,9 @@ function FeedSkeleton() {
             <CardContent>
                 <Box sx={{ fontSize: 14, whiteSpace: 'pre-line', wordWrap: 'break-word' }} color="text.secondary">
                     <Skeleton variant="text" sx={{ fontSize: '2rem' }} />
-
                 </Box>
             </CardContent>
         </Card>
-
     )
 }
   
