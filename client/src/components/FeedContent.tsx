0617b6574d21eeae5ef87f3e0a70ee3a03778083
Dipokal HHJ <hhj@devent.kr>
2023-07-29 18:06:04 +0900

    refactor: add api structure

--- client/src/components/FeedContent.tsx
+++ client/src/components/FeedContent.tsx
@@ -4,45 +4,23 @@ import { TextField, Button, Stack, Grid, Card, CardContent, Typography, Box, Ske
 import { Popup } from './Alert'
 import { FeedBody } from './Feed'
 import { useDispatch, useSelector } from 'react-redux';
+import { FeedAPI } from "../api";
 
-import axios from "axios"
-import Cookies from 'js-cookie'
 import Navbar from './Navbar'
 
 
-async function getFeed(idx) {
-    let response = await axios.request({
-        method: 'get',
-        url: `/api/feeds/${idx}`,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        responseType: 'json'
-    })
-
-
-    return response.data
-}
-
 function FeedContent() {
-    const dispatch = useDispatch();
-
-    const isLogin = useSelector((state: any) => state.auth.isLogin);
-    const userId = useSelector((state: any) => state.auth.userId);
     const [content, setContent] = useState([{idx: 0, content:'', owner: '', date: '', type: 1}])
-
 
     const fetchFeed = async () => {
         const feedIdx = Number(location.pathname.split('/')[2])
-        const getFeedData = await getFeed(feedIdx)
+        const getFeedData = await FeedAPI.getFeed(feedIdx, {})
 
         setContent([{...getFeedData.data.result[0]}])
     }
 
     useEffect(() => {
         fetchFeed()
-
-
     }, [])
 
     return (
