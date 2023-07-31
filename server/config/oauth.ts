bd6687c83162e46a4868e6bc011e0b21a56c168a
Dipokal HHJ <hhj@devent.kr>
2023-07-31 21:16:25 +0900

    config: remove google id

--- server/config/oauth.ts
+++ server/config/oauth.ts
@@ -1,5 +1,5 @@
 export default 
 {
-    "GOOGLE_CLIENT_ID": "353534757059-j7lntvmdj3lob0u98lbhqes8u9vafq9m.apps.googleusercontent.com",
-    "GOOGLE_CLIENT_SECRET": "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"
+    "GOOGLE_CLIENT_ID": "",
+    "GOOGLE_CLIENT_SECRET": ""
 }
